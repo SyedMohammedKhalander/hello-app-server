644e0f3cd797c34c9ea517c57273ee232bf8a1f0
SyedMohammedKhalander <syedkhalandar80@gmail.com>
2023-08-09 06:24:46 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"Hello, world!")
+
+server_address = ("", 8000)
+httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+httpd.serve_forever()
